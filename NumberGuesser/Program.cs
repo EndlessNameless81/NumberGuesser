57df6b5b842df9a1fd24f451ff0e715fff20cf3e
EndlessNameless <audioslave81@yahoo.com>
2020-01-31 13:23:10 +0300

    начало

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace NumberGuesser
 {
@@ -6,7 +7,39 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");//
+            Random rnd = new Random();
+
+            int compNumber, userNumder;
+            string mgr = "CompNumber = {0} | UserNumber = {1}";
+            bool inGame = false;
+
+            Console.Title = "Игра угадай число";
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Компьютер загадал число от 0 до 100. Попробуй отгадать у тебя 5 попыток");
+            Console.WriteLine("Хотите сыграть? Y/N");
+
+            string questChar = Console.ReadLine();
+
+            switch (questChar)
+            {
+                case "Y":
+                    inGame = true;
+                    break;
+                case "N":
+                    inGame = false;
+                    break;
+            }
+
+            while (inGame)
+            {
+                compNumber = rnd.Next(100);
+                userNumder = Int32.Parse(Console.ReadLine());
+
+                //Output
+                Console.WriteLine(mgr, compNumber, userNumder);
+                Console.ReadLine();
+            }
         }
     }
 }
